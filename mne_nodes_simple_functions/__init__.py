2586d10f39f04f088bdecc0c9f77480328fd1d2f
Martin G. Schulz <46245704+marsipu@users.noreply.github.com>
2026-09-20 18:57:14 +0200

    fix plugin-name

--- mne_nodes_simple_functions/__init__.py
+++ mne_nodes_simple_functions/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 
-PLUGIN_NAME = "simple-functions"
+PLUGIN_NAME = "simple_functions"
 PLUGIN_GITHUB = "https://github.com/marsipu/mne-nodes_simple-functions"
 CONFIG_PATH = Path(__file__).parent / "simple_functions_config.json"
 SCRIPT_PATH = Path(__file__).parent / "simple_functions.py"
